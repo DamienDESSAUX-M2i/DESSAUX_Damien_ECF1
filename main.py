f9aa3f6d999686605018396c3cdbe4ddd815d531
Damien DESSAUX <damien.dessaux@labom2iformation.fr>
2026-01-05 09:36:13 +0100

    Set up logger.

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-def main():
-    print("Hello from dessaux-damien-ecf1!")
+import logging
+from pathlib import Path
+
+from src.utils.logger import set_up_logger
+
+DIR_PATH = Path(__file__).resolve()
+
+
+def main() -> None:
+    log_dir = DIR_PATH / "logs"
+    if not log_dir.exists():
+        log_dir.mkdir()
+    logger_path = log_dir / "app.log"
+    logger: logging.Logger = set_up_logger(
+        name="app", path=logger_path, level=logging.INFO
+    )
 
 
 if __name__ == "__main__":
---
+++ src/utils/__init__.py
@@ -0,0 +1,3 @@
+from .logger import set_up_logger
+
+__all__ = ["set_up_logger"]
